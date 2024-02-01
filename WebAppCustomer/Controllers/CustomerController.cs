e97eb9a34454e2ca0718220f68b0532cfd31e467
Nayemsyed <sdnayem04112000@gmail.com>
2024-02-01 15:38:13 +0530

    Add project files.

---
+++ WebAppCustomer/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppCustomer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ViewBag.msg = "Customer Home Page";
+            return View();
+        }
+        public ActionResult CustomerList()
+        {
+            List<string> list = new List<string>()
+            {
+             "sita",
+             "Gita",
+             "Riya",
+             "Rithu"
+            };
+            ViewBag.CustomerList = list;
+            return View();
+        }
+    }
+}
---
+++ WebAppCustomer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppCustomer.MvcApplication" Language="C#" %>
